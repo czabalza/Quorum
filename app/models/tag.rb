1fc3679f44317a9c01460dfabc56498bbe92969c
Christina Zabalza <christinazabalza1991@gmail.com>
2015-04-09 18:09:11 -0700

    add uniqueness constraint to tag model

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,5 +1,5 @@
 class Tag < ActiveRecord::Base
-  validates :topic, presence: true
+  validates :topic, presence: true, uniqueness: true
 
   has_many :subscriptions, dependent: :destroy
   has_many :taggings, dependent: :destroy
